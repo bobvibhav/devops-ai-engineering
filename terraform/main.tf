bf943dce3542ac1ce1152ed382034ee88be7667c
Vibhav <bob.vibhav@gmail.com>
2026-07-26 17:29:28 +0530

    Terraform Learning

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+
+    required_providers {
+        aws = {
+
+            source  = "hashicorp/aws"
+            version = "~> 5.0"
+        }
+    }
+
+}
+
+provider "aws"{
+    region = "ap-south-1"
+    }
+
+resource "aws_s3_bucket" "my_first_bucket" {
+  bucket = "vibhav-terraform-demo-bucket-2026"
+}
