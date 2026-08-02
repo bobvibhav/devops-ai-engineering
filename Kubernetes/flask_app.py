b3443e2133cd61e9d756bb5fc0c7d4aec8d9772a
Vibhav <bob.vibhav@gmail.com>
2026-08-02 16:34:06 +0530

    Learning Kubernetes

---
+++ Kubernetes/flask_app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    hostname = socket.gethostname()
+    return f"Hello from Pod: {hostname}\n"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
